329aef7dbdf8f1928d0610f080a3e204d9989578
nurik <moldanazarov.n@phystech.edu>
2025-07-25 02:24:53 +0300

    ура

--- src/utils/os.go
+++ src/utils/os.go
@@ -13,14 +13,14 @@ func DownloadFile(url, fileName string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("error in download file: [file url: %s] %v", url, err)
 	}
-	defer func() { _ = resp.Body.Close() }()
+	defer CloserErrorHandle(resp.Body, "error in closing response body")
 
 	filePath := filepath.Join("downloads", fileName+".mp3")
 	out, err := os.Create(filePath)
 	if err != nil {
 		return "", fmt.Errorf("error in create file: [file name: %s] %v", fileName, err)
 	}
-	defer out.Close()
+	defer CloserErrorHandle(out, "error in closing output file")
 
 	if _, err := io.Copy(out, resp.Body); err != nil {
 		return "", fmt.Errorf("error in copy response body: %v", err)
